99141d4b1e2d78b630961bd5e12cb0d3f4c2b2ea
DaryPet <d7akkord@gmail.com>
2024-02-27 17:19:49 +0100

    HW_12_all

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,14 +4,14 @@ const BASE_URL = 'https://pixabay.com/api/';
 const IMAGE_TYPE = 'photo';
 const ORIENTATION = 'horizontal';
 const SAFESEARCH = true;
-// const PER_PAGE = 15;
+const PER_PAGE = 15;
 
 import axios from 'axios';
 
-export async function getImages(searchText) {
+export async function getImages(searchText, page = 1) {
   try {
     const QUERY = encodeURIComponent(searchText);
-    const requestURL = `${BASE_URL}?key=${KEY}&q=${QUERY}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFESEARCH}`;
+    const requestURL = `${BASE_URL}?key=${KEY}&q=${QUERY}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFESEARCH}&per_page=${PER_PAGE}&page=${page}`;
 
     const response = await axios.get(requestURL);
     if (response.status !== 200) {
--- src/main.js
+++ src/main.js
@@ -2,48 +2,67 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import { getImages } from './js/pixabay-api.js';
-import { displayImages } from './js/render-functions.js';
-import { displayErrorMessage } from './js/render-functions.js';
+import { displayImages, displayErrorMessage } from './js/render-functions.js';
 
 const form = document.querySelector('#search-form');
 const input = document.querySelector('#search-input');
 const gallery = document.querySelector('.gallery');
 const loader = document.getElementById('loader');
 const loadMoreBtn = document.querySelector('.btn-load-more');
+let searchText = '';
+let currentPage = 1;
 
 form.addEventListener('submit', async function (event) {
   event.preventDefault();
 
-  const searchText = input.value.trim();
+  searchText = input.value.trim();
 
   if (searchText === '') {
-    displayErrorMessage('Please fill imput');
+    displayErrorMessage('Please fill input');
     return;
   }
+
   loader.classList.remove('is-hidden');
 
   try {
-    const data = await getImages(searchText);
-    if (data.hits.length === 0) {
-      displayErrorMessage(
-        'Sorry, there are no images matching your search query. Please try again!'
-      );
-      return;
-    }
-    displayImages(data.hits, gallery);
-    input.value = '';
+    const data = await getImages(searchText, 1);
+    currentPage = 1;
+    handleImageData(data);
   } catch (error) {
     console.error('Error during search:', error);
-    displayErrorMessage('Error during search');
+    displayErrorMessage('Error');
   } finally {
     loader.classList.add('is-hidden');
   }
 });
 
-// loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.addEventListener('click', async function () {
+  loader.classList.remove('is-hidden');
 
-// function onLoadMore() {
-//   loader.classList.remove('is-hidden');
-// }
+  try {
+    const data = await getImages(searchText, currentPage + 1);
+    currentPage++;
+    handleImageData(data);
+  } catch (error) {
+    console.error('Error during loading more images:', error);
+    displayErrorMessage('Error loading more images');
+  } finally {
+    loader.classList.add('is-hidden');
+  }
+});
+
+function handleImageData(data) {
+  if (data.hits.length === 0) {
+    displayErrorMessage(
+      'Sorry, there are no images matching your search query. Please try again!'
+    );
+    return;
+  }
+
+  if (loadMoreBtn.classList.contains('is-hidden')) {
+    loadMoreBtn.classList.remove('is-hidden');
+  }
+
+  displayImages(data.hits, gallery);
+}
